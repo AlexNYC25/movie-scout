b98f1ffdfe3134028d801c5ff29617d8653daaf5
Alexis Montes <montes.alexis25@gmail.com>
2022-01-27 21:13:48 -0500

    fixed promise bugs

--- helpers/tmdb.js
+++ helpers/tmdb.js
@@ -1,32 +1,33 @@
 import axios from 'axios';
 
 
-let getMediaDetails = async (id, media_type) => {
+let getMediaDetails = (id, media_type) => {
     let apiMediaType = media_type === 'movie' ? 'movie' : 'tv';
 
-
-    let promise = axios.get(`https://api.themoviedb.org/3/${apiMediaType}/${id}?api_key=${process.env.TMDB_API_KEY}&language=en-US`)
-        .then(response => {
-            return response.data;
+    return axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.TMDB_API_KEY}&language=en-US`)
+        .then(res => {
+            return res.data;
         })
-        .catch(error => {
-            console.log(error);
+        .catch(err => {
+            console.log(err);
         })
-
-    return promise;
-
+    
+    
 }
 
 export async function getAllMediaDetails(arr){
     let promiseArr = [];
-    console.log(arr.length)
+    console.log(arr);
+    //arr = [{id:1725, media_type:'movie'}, {id:1726, media_type:'movie'}]
+    
     for(let i = 0; i < arr.length; i++){
         promiseArr.push(getMediaDetails(arr[i].id, arr[i].media_type));
     }
+    let temp = Promise.allSettled(promiseArr);
 
-    Promise.all(promiseArr)
-        .then(result => {
-            return result;
-        })
+    const res = await temp;
+    return res;
+    
+
 }
 
--- models/mcu.js
+++ models/mcu.js
@@ -2,12 +2,34 @@ import db from "../db/connection.js";
 
 import { getAllMediaDetails } from "../helpers/tmdb.js";
 
-export function getAllMcuMedia() {
-    db.query('SELECT * FROM media WHERE category = $1;', ['MCU'], (err, res) => {
+// query db for all media where category is MCu returning it as a promise
+const getAllMcuMediaFromDb = async () => {
+    try {
+        const res = await db
+            .query(
+                `SELECT * FROM media WHERE category = 'MCU'`
+            );
         
-        getAllMediaDetails(res.rows).then(result => {
-            return result;
-        })
-    });
+        return res.rows;
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+// use getAllMediaDetails to get all media details for each MCU movie as a promise
+const getAllMcuMediaDetails = async () => {
+    try {
+        const res = await getAllMediaDetails(await getAllMcuMediaFromDb());
+        
+        return res;
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+
+export function getAllMcuMedia() {
+    console.log(getAllMcuMediaDetails());
+    return getAllMcuMediaDetails();
     
 }
