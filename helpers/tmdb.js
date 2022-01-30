60812220230effc32dca9f6043fe8970435a4141
Alexis Montes <montes.alexis25@gmail.com>
2022-01-29 23:09:52 -0500

    cleaned up tmdb helper

--- helpers/tmdb.js
+++ helpers/tmdb.js
@@ -1,24 +1,21 @@
 import axios from 'axios';
 
 
-let getMediaDetails = (id, media_type) => {
-    let apiMediaType = media_type === 'movie' ? 'movie' : 'tv';
+let getMediaDetails = async (id, media_type) => {
+    let apiMediaType = media_type == 'movie' ? 'movie' : 'tv';
 
-    return axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.TMDB_API_KEY}&language=en-US`)
-        .then(res => {
-            return res.data;
-        })
-        .catch(err => {
-            console.log(err);
-        })
+    try {
+        const res = await axios.get(`https://api.themoviedb.org/3/${apiMediaType}/${id}?api_key=${process.env.TMDB_API_KEY}&language=en-US`);
+        return res.data;
+    } catch (err) {
+        console.log(err);
+    }
     
     
 }
 
 export async function getAllMediaDetails(arr){
     let promiseArr = [];
-    console.log(arr);
-    //arr = [{id:1725, media_type:'movie'}, {id:1726, media_type:'movie'}]
     
     for(let i = 0; i < arr.length; i++){
         promiseArr.push(getMediaDetails(arr[i].id, arr[i].media_type));
