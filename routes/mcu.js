ca14f6698bd1c9e84a40df7ef5b87dacac199ef6
Alexis Montes <montes.alexis25@gmail.com>
2022-01-30 21:26:00 -0500

    set up additional routes for geting mcu media

--- routes/mcu.js
+++ routes/mcu.js
@@ -1,15 +1,45 @@
 import express from "express";
 const router = express.Router();
 
-import {getAllMcuMedia} from '../models/mcu.js';
+import {getAllMcuMedia, getAllMcuMovies, getAllMcuTvShows} from '../models/mcu.js';
 
-/* GET users listing. */
+/* GET all mcu media objects(movies and tvShows). */
 router.get("/", function (req, res, next) {
+  try{
+    getAllMcuMedia().then(data => {
+      res.status(200).json(data);
+    });
+  } catch (err) {
+    res.status(500).json({
+      error: err.message
+    });
+  }
+});
 
-  getAllMcuMedia().then(data => {
-    res.status(200).json(data);
-  });
-  
-  });
+// GET all mcu movies objects.
+router.get('/movies', function(req, res, next) {
+  try{
+    getAllMcuMovies().then(data => {
+      res.status(200).json(data);
+    });
+  } catch (err) {
+    res.status(500).json({
+      error: err.message
+    });
+  }
+});
+
+// GET all mcu tv shows objects.
+router.get('/tvShows', function(req, res, next) {
+  try{
+    getAllMcuTvShows().then(data => {
+      res.status(200).json(data);
+    });
+  } catch (err) {
+    res.status(500).json({
+      error: err.message
+    });
+  }
+});
   
 export default router;
