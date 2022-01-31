4d21cfe81ed430f86ac4585a52b343eb897bd6a4
Alexis Montes <montes.alexis25@gmail.com>
2022-01-30 21:22:37 -0500

    added functions to get specific media type

--- models/mcu.js
+++ models/mcu.js
@@ -16,7 +16,33 @@ const getAllMcuMediaFromDb = async () => {
     }
 }
 
-// use getAllMediaDetails to get all media details for each MCU movie as a promise
+// query db for all MCU media that is a movie
+const gelAllMcuMoviesFromDb = async () => {
+    try{
+        const res = await db
+            .query(
+                `SELECT * FROM media WHERE category = 'MCU' AND media_type = 'movie'`
+            );
+        return res.rows;
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+// query db for all MCU media that is a tv show
+const getAllMcuTvShowsFromDb = async () => {
+    try{
+        const res = await db
+            .query(
+                `SELECT * FROM media WHERE category = 'MCU' AND media_type = 'tv_show'`
+            );
+        return res.rows;
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+// use getAllMediaDetails to get all media details for each MCU media from TMDB api
 const getAllMcuMediaDetails = async () => {
     try {
         const res = await getAllMediaDetails(await getAllMcuMediaFromDb());
@@ -27,8 +53,40 @@ const getAllMcuMediaDetails = async () => {
     }
 }
 
+// use getAllMediaDetails to get all media details for each MCU media from TMDB api that is a movie
+const getAllMcuMoviesDetails = async () => {
+    try {
+        const res = await getAllMediaDetails(await gelAllMcuMoviesFromDb());
+        
+        return res;
+    } catch (err) {
+        console.log(err);
+    }
+}
 
+// use getAllMediaDetails to get all media details for each MCU media from TMDB api that is a tv show
+const getAllMcuTvShowsDetails = async () => {
+    try {
+        const res = await getAllMediaDetails(await getAllMcuTvShowsFromDb());
+
+        return res;
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+// export function to get all MCU media
 export function getAllMcuMedia() {
     return getAllMcuMediaDetails();
     
+}
+
+// export function to get all MCU movies
+export function getAllMcuMovies() {
+    return getAllMcuMoviesDetails();
+}
+
+// export function to get all MCU tv shows
+export function getAllMcuTvShows() {
+    return getAllMcuTvShowsDetails();
 }
